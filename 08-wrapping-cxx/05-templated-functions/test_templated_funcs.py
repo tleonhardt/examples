1c03976585ff0c6aaea3b89e68b2b7c8912db3a8
Todd Leonhardt <todd.leonhardt@gmail.com>
2017-01-07 11:53:00 -0500

    Worked through example of wrapping C++ templated functions with Cython

---
+++ 08-wrapping-cxx/05-templated-functions/test_templated_funcs.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+pytest unit test file for RNG Cython code
+"""
+import templated_funcs as tf
+
+
+def test_min_long():
+    assert tf.min(5, 7) == 5
+
+
+def test_min_double():
+    assert tf.min(2.3, 2.1) == 2.1
+
+
+def test_max_long():
+    assert max(23, 42) == 42
+
+
+def test_max_double():
+    assert max(3.14159, 2.7) == 3.14159
